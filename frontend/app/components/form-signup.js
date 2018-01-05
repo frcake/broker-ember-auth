e421601976afce2aa3ef0dc8f62c7741f5123085
frcake <mpdevcake@gmail.com>
2018-01-05 17:38:00 +0200

    Refactors Sign Up action with ember concurrency

--- frontend/app/components/form-signup.js
+++ frontend/app/components/form-signup.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
 export default Ember.Component.extend({
 	tagName: '',
 	session: Ember.inject.service('session'),
 
+
+
 	actions: {
-		submit() {
+		propagateSaveAction() {
 			let user = this.get('user')
-			user.viewer = true
+			//user.viewer = true
 			this.attrs.triggerSave(user);
 		}
 	}
--- frontend/app/controllers/users/sign-up.js
+++ frontend/app/controllers/users/sign-up.js
@@ -1,21 +1,32 @@
 // TODO: it will be removed
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
 export default Ember.Controller.extend({
 	session: Ember.inject.service('session'),
+
+ saveNewUser: task(function *(newUser){
+  //yield the promise response or catch any errors
+  try {
+    yield newUser.save();
+  } catch (error) {
+   	this.set('errorMessage', error)
+  }
+  //everything's fine so trigger the next action and await it
+  yield this.get('authenticateNewUser').perform(newUser);
+ }),
+
+ authenticateNewUser: task(function*(newUser){
+   try {
+     yield this.get('session').authenticate('authenticator:devise',newUser.get('email'), newUser.get('password'))
+   } catch (reason) {
+    	this.set('errorMessage', reason.error || reason);
+   }
+ }),
+
 	actions: {
-		save(user) {
-			let newUser = user;
-			newUser.save().catch((error) => {
-					this.set('errorMessage', error)
-				})
-				.then(() => {
-					this.get('session')
-						.authenticate('authenticator:devise',
-							newUser.get('email'), newUser.get('password'))
-						.catch((reason) => {
-							this.set('errorMessage', reason.error || reason);
-						});
-				})
+		triggerSaveChain(user) {
+   this.get('saveNewUser').perform(user)
 		}
 	}
 });
